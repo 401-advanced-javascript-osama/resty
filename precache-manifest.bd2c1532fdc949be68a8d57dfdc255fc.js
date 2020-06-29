d959d2ca9022ad16800a8b21a4e32bf0d5456b24
osamamousa204 <runner@fv-az40.5xlnah3oxifurmbu52f4h2herc.bx.internal.cloudapp.net>
2020-06-29 20:51:58 +0000

    Updates

---
+++ precache-manifest.bd2c1532fdc949be68a8d57dfdc255fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f14a64fb567174ac0e113de76a70e8",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "70880b8df657080834f6",
+    "url": "/resty/static/css/main.718f2a6e.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/resty/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70880b8df657080834f6",
+    "url": "/resty/static/js/main.24b7dd8d.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
