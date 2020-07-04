b50e4354933d885fb8aadd5f4892631a24307688
osamamousa204 <osamamousa204@gmail.com>
2020-07-04 15:51:54 +0300

    lab29

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,34 +1,89 @@
 import React from 'react';
 import './form.scss';
+import { If, Then, Else } from '../if';
+
 class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      methode: '',
+      method: '',
       url: '',
+      body : {},
       request: {}
     };
   }
   handleSubmit = async (e) => {
+    // console.log('dddddddddd', this.props.fiilForm);
+    console.log('hhhhh', this.state.url);
+    
     e.preventDefault();
-    if (this.state.url && this.state.methode) {
-        try {
-          const raw = await fetch(`${this.state.url}`);
-          const data = await raw.json();
-          let head ;
-          raw.headers.forEach(value =>{
-            head = { 'Content-Type': value }
-          })          
-          let results = {
-            Headers: head,
-            Response: data
+    this.props.toggleLoading();
+    console.log(this.state.url , this.state.methode);
+    if (this.state.url && this.state.methode) {  
+      
+      switch (this.state.methode) {
+        case 'get':
+          try {
+            let raw = await fetch(this.state.url);
+            let data = await raw.json();
+            let head;
+            raw.headers.forEach(value => {
+              head = { 'Content-Type': value }
+            })
+            let results = {
+              Headers: head,
+              Response: data
+            }
+            this.props.handler(results);
+            this.props.toggleLoading();
+            this.props.setHistory(this.state.methode,this.state.url,this.state.body);
+          } catch (e) {
+            console.log(e);
           }
-          this.props.handler(results);
-        } catch (e) {
-          console.log(e);
-        }
+          break;
+        case 'post':
+        case 'put':
+          if (this.state.body) {
+            fetch(this.state.url, {
+              method: `${this.state.methode}`,
+              mode: 'cors',
+              headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+              },
+              body: this.state.body
+            })
+              .then(data => data.json()).then(results => {
+               this.props.handler(results);
+            this.props.toggleLoading();
+            this.props.setHistory(this.state.methode,this.state.url,this.state.body);
+
+              })
+          } else {
+            alert('please Enter the body');
+          }
+          break;
+        case 'delete':
+          fetch(this.state.url, {
+            method: `${this.state.methode}`,
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*'
+            },
+          })
+            .then(() => {
+              this.props.handler({results:'Item Deleted ...'});
+            this.props.toggleLoading();
+            this.props.setHistory(this.state.methode,this.state.url,this.state.body);
+
+            })
+      }
+
     }
-  };
+  }
+    
+  
 
   handleChangeUrl = (e) => {
     const url = e.target.value;
@@ -37,31 +92,101 @@ class Form extends React.Component {
   handleChangeMethod = (e) => {
     const methode = e.target.value;
     this.setState({ methode })
-  };
+  }
 
-  render() {
+  handleBody = e =>{
+    const body = e.target.value;
+    this.setState({body})
+  }
+
+  render () {
+
+  
     return (
       < main className="main">
-        <form onSubmit={this.handleSubmit}>
+         <If condition={this.props.fiilForm.url}>
+          <Then>
+          <form onSubmit={this.handleSubmit}>
+          <label className="url">
+            <span>URL:</span>
+            <input type="text" id="url" onChange={this.handleChangeUrl}   value={this.props.fiilForm.url} />
+            <input id="submit" type="submit" value ="GO!"/>
+          </label>
+          <label className="method">
+          <If condition={this.props.fiilForm.method == 'get'}>
+          <Then>
+          <input onChange={this.handleChangeMethod} defaultChecked   type="radio" id="get" name="methode" value="get" />
+            <label>GET</label>
+          </Then>
+          <Else>
+          <input onChange={this.handleChangeMethod}   type="radio" id="get" name="methode" value="get" />
+          <label>GET</label>
+          </Else>
+        </If>
+        <If condition={this.props.fiilForm.method == 'post'}>
+          <Then>
+          <input onChange={this.handleChangeMethod} defaultChecked type="radio" id="post" name="methode" value="post" />
+            <label>POST</label>
+          </Then>
+          <Else>
+          <input onChange={this.handleChangeMethod}  type="radio" id="post" name="methode" value="post" />
+            <label>POST</label>
+          </Else>
+        </If>
+        <If condition={this.props.fiilForm.method == 'put'}>
+          <Then>
+          <input onChange={this.handleChangeMethod} defaultChecked type="radio" id="put" name="methode" value="put" />
+            <label>PUT</label>
+          </Then>
+          <Else>
+          <input onChange={this.handleChangeMethod}  type="radio" id="put" name="methode" value="put" />
+            <label>PUT</label>
+          </Else>
+        </If>
+        <If condition={this.props.fiilForm.method == 'delete'}>
+          <Then>
+          <input onChange={this.handleChangeMethod} defaultChecked type="radio" id="delete" name="methode" value="delete" />
+            <label>DELETE</label>
+          </Then>
+          <Else>
+          <input onChange={this.handleChangeMethod} type="radio" id="delete" name="methode" value="delete" />
+            <label>DELETE</label>
+          </Else>
+        </If>
+            <br/>
+            <label> Body :
+          <textarea rows="15" cols="50" className="body" value ={this.props.fiilForm.body} > </textarea></label>
+          </label>
+        </form>
+          </Then>
+          <Else>
+          <form onSubmit={this.handleSubmit}>
           <label className="url">
             <span>URL:</span>
             <input type="text" id="url"  onChange={this.handleChangeUrl} />
             <input id="submit" type="submit" value ="GO!"/>
           </label>
           <label className="method">
-            <input onChange={this.handleChangeMethod} type="radio" id="get" name="methode" value="GET" />
+            <input onChange={this.handleChangeMethod} type="radio" id="get" name="methode" value="get" />
             <label>GET</label>
-            <input onChange={this.handleChangeMethod} type="radio" id="post" name="methode" value="POST" />
+            <input onChange={this.handleChangeMethod} type="radio" id="post" name="methode" value="post" />
             <label>POST</label>
-            <input onChange={this.handleChangeMethod} type="radio" id="put" name="methode" value="PUT" />
+            <input onChange={this.handleChangeMethod} type="radio" id="put" name="methode" value="put" />
             <label>PUT</label>
-            <input onChange={this.handleChangeMethod} type="radio" id="delete" name="methode" value="DELETE" />
+            <input onChange={this.handleChangeMethod} type="radio" id="delete" name="methode" value="delete" />
             <label>DELETE</label>
+            <br/>
+            <label> Body :
+          <textarea rows="15" cols="50" className="body" onChange={this.handleBody} > </textarea></label>
           </label>
         </form>
+          </Else>
+        </If>
+        
       </main>
-    );
+    )
+  
+    }
   }
-}
 
 export default Form;
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactJson from 'react-json-view'
+import { If, Then, Else } from '../if';
+import './loader.scss';
+
 function Result(props) {
+  console.log(props.loading);
   return (
     <>
-    <ReactJson src={props.result} theme="monokai" />
+    {/* <ReactJson src={props.result} theme="monokai" /> */}
+    {/* <img src="https://i.pinimg.com/originals/72/f1/a4/72f1a425e3ea3505929a30b8adabefb8.gif" className={`loading-${props.loading}`}/> */}
+    {/* <If condition={!props.loading}> */}
+          {/* <Then> */}
+          <ReactJson id= "output" src={props.result}  /> 
+          {/* </Then> */}
+          {/* <Else>
+          <img src="https://i.pinimg.com/originals/72/f1/a4/72f1a425e3ea3505929a30b8adabefb8.gif" />
+          </Else> */}
+        {/* </If> */}
+        {/* <div className={`loading-${!props.loading}`}  id="output">
+            <ReactJson src={props.result} />
+        </div> */}
     </>
   );
 }
