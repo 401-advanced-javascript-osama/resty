a4772dc47a594940b8d1054db71b9634398b9351
osamamousa204 <runner@fv-az71.rdiviglcgggunn5ncau1dczqlb.bx.internal.cloudapp.net>
2020-08-10 17:39:14 +0000

    Updates

---
+++ precache-manifest.ad82c7919c05ba6929c0524dd47b62de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fe0c1e088a99e1aac9d42c40d5fb94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07a9a0b6161a1e5dbef6",
+    "url": "/static/css/main.acaf9942.chunk.css"
+  },
+  {
+    "revision": "63fe1c9ddd40572a52d4",
+    "url": "/static/js/2.f391f0bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f391f0bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a9a0b6161a1e5dbef6",
+    "url": "/static/js/main.2b25d55a.chunk.js"
+  },
+  {
+    "revision": "e7b4ea4f970612a52c32",
+    "url": "/static/js/runtime-main.3acf32b8.js"
+  }
+]);
